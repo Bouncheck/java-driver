8d89b3c74df7aa8ff463d168562aed031de08183
Wojciech Bączkowski <wojciech.baczkowski@scylladb.com>
2022-06-13 01:17:29 +0200

    actions 3

--- driver-core/src/main/java/com/datastax/driver/core/RequestHandler.java
+++ driver-core/src/main/java/com/datastax/driver/core/RequestHandler.java
@@ -831,7 +831,7 @@ class RequestHandler {
                           toPrepare.getQueryString()));
                 }
 
-                // trigger actions 2
+                // trigger actions 3
                 if (retriesByUnprepared > SPECULATIVE_EXECUTION_MAX_REPREPARES) {
                   connection.release();
                   String msg =
